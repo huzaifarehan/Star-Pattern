2db31bd4305d3092a78a1eacba6b1ac01cbefcb7
unknown <huzaifarehan001@gmail.com>
2021-08-29 04:04:29 -0700

    huzaifa
    test deploy

---
+++ index.js
@@ -0,0 +1,8 @@
+function imgslider(anything){
+   
+   document.querySelector('.pepsi').src = anything;
+}
+function bgColorChange(color){
+    const container = document.querySelector('body');
+    container.style.background = color;
+}
